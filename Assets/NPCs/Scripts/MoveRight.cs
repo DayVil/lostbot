7d2965a00dea3d68772f32cbcd04745e1aa6316e
Neparshath-coder <Kentch.Tahm@googlemail.com>
2023-10-23 17:21:44 +0200

    Update MoveRight.cs

--- Assets/NPCs/Scripts/MoveRight.cs
+++ Assets/NPCs/Scripts/MoveRight.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveRight : MonoBehaviour
 {
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class MoveRight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position+= Vector3.right * speed * Time.deltaTime;
     }
 }
