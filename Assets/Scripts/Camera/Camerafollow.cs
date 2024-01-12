f8a3ef8793bdf06e48e59459eb1be1f0fa760f9c
Neparshath-coder <Kentch.Tahm@googlemail.com>
2024-01-12 14:22:00 +0100

    fix

--- Assets/Scripts/Camera/Camerafollow.cs
+++ Assets/Scripts/Camera/Camerafollow.cs
@@ -7,13 +7,20 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public float yOffset; // Vertical offset
     private readonly Manager _manager = Manager.GetInstance;
+    public float maxYDepth = -2;
+    
 
     void FixedUpdate()
     {
         if (_manager.StartGame)
         {
+            var targetYPos = target.position.y;
+            if(targetYPos < maxYDepth) targetYPos = maxYDepth;
+
             // Calculate the target position in world space
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
+            Vector3 targetPosition = new Vector3(target.position.x, targetYPos + yOffset, transform.position.z);
+
+            
 
             // Smoothly follow the target in x and y directions
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
